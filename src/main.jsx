8b7255be5a0145b55ce33e503d1e4e3ea203bbf1
Rami Moshe <rmoshe@salesforce.com>
2025-08-07 12:29:17 +0300

    Fix React Router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
+const basename = import.meta.env.PROD ? '/ym-utils-website' : '/'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
